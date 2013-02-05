c8b63a9072f6f835cfcb1ced5985f379f10e7bd5
Justin Salmon <jsalmon@cern.ch>
2013-02-05 09:23:33 +0100

    Modify copyright file headers

--- agent/src/ch/cern/dss/teamcity/agent/AbiCheckerCommandLine.java
+++ agent/src/ch/cern/dss/teamcity/agent/AbiCheckerCommandLine.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2012-2013 by European Organization for Nuclear Research (CERN)
+ * Author: Justin Salmon <jsalmon@cern.ch>
+ *
+ * This file is part of the ABI Compatibility Checker (ACC) TeamCity plugin.
+ *
+ * ACC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ACC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ACC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ch.cern.dss.teamcity.agent;
 
 import ch.cern.dss.teamcity.agent.util.SimpleLogger;
